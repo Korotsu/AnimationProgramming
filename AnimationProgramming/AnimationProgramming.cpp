fd67248364cf059fd74cf17a0c51dfc96cfbed7f
s.amara <s.amara@student.isartdigital.com>
2020-12-15 12:12:54 +0100

    First test with Engine.h functions

--- AnimationProgramming/AnimationProgramming.cpp
+++ AnimationProgramming/AnimationProgramming.cpp
@@ -5,16 +5,25 @@
 
 class CSimulation final : public ISimulation
 {
+
+
 	virtual void Init() final
 	{
-		int spine01 =	GetSkeletonBoneIndex("spine_01");
-		int spineParent = GetSkeletonBoneParentIndex(spine01);
+		const size_t boneCount		= GetSkeletonBoneCount();
+		int spine01					= GetSkeletonBoneIndex("spine_01");	// Bone parent
+		int spineParent				= GetSkeletonBoneParentIndex(spine01);
 		const char* spineParentName = GetSkeletonBoneName(spineParent);
 
 		float posX, posY, posZ, quatW, quatX, quatY, quatZ;
 		const size_t keyCount = GetAnimKeyCount("ThirdPersonWalk.anim");
 		GetAnimLocalBoneTransform("ThirdPersonWalk.anim", spineParent, keyCount / 2, posX, posY, posZ, quatW, quatX, quatY, quatZ);
+
+		for (size_t i{0u}; i < boneCount; ++i)
+		{
+			printf("Bone #%d: %s\n", i, GetSkeletonBoneName(i));
+		}
 		
+
 		printf("Spine parent bone : %s\n", spineParentName);
 		printf("Anim key count : %ld\n", keyCount);
 		printf("Anim key : pos(%.2f,%.2f,%.2f) rotation quat(%.10f,%.10f,%.10f,%.10f)\n", posX, posY, posZ, quatW, quatX, quatY, quatZ);
@@ -30,7 +39,6 @@ class CSimulation final : public ISimulation
 
 		// Z axis
 		DrawLine(0, 0, 0, 0, 0, 100, 0, 0, 1);
-
 	}
 };
 
@@ -40,5 +48,5 @@ int main()
 	CSimulation simulation;
 	Run(&simulation, 1400, 800);
 
-    return 0;
+	return 0;
 }
